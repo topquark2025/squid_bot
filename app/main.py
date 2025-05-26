fe6a79de1f72f288c5aa909df7557d2226179c74
topquark2025 <creatortopquark@gmail.com>
2025-05-26 15:47:22 +0900

    main.py 업데이트

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from gps_helper import GPSListener
+from utils import send_location_to_server
 
+class MyApp(App):
+    def build(self):
+        self.label = Label(text="GPS 추적 중...")
+        self.gps = GPSListener(callback=self.on_location)
+        self.gps.start()
+        return self.label
+
+    def on_location(self, lat, lon):
+        self.label.text = f"위치: {lat}, {lon}"
+        send_location_to_server(lat, lon)
+
+if __name__ == '__main__':
+    MyApp().run()
