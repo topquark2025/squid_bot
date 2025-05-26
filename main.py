c0e4c9d6547aab7e66817f4425fb532c74921943
topquark2025 <creatortopquark@gmail.com>
2025-05-26 11:06:14 +0900

    main.py 업데이트

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print(" ")
+from server import run_forever #서버
+
+import discord
+from discord.ext import commands , tasks
+from discord.ui import View, Select
+from discord import app_commands
+
+from dotenv import load_dotenv
+
+import aiohttp
+import asyncio
+from selectolax.parser import HTMLParser
+import os
