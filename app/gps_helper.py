271d26b58c0bacc6b3298eea4ba5093d209ef8a7
topquark2025 <creatortopquark@gmail.com>
2025-05-26 15:47:57 +0900

    gps_helper.py 업데이트

--- app/gps_helper.py
+++ app/gps_helper.py
@@ -1,1 +1,15 @@
+from plyer import gps
 
+class GPSListener:
+    def __init__(self, callback):
+        self.callback = callback
+
+    def start(self):
+        gps.configure(on_location=self.on_location)
+        gps.start(minTime=1000, minDistance=1)
+
+    def on_location(self, **kwargs):
+        lat = kwargs.get('lat')
+        lon = kwargs.get('lon')
+        if self.callback:
+            self.callback(lat, lon)
