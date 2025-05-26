76e7491388bbd8553da25d4884954dc2a0411b8c
topquark2025 <creatortopquark@gmail.com>
2025-05-26 11:10:44 +0900

    main.py 업데이트

--- main.py
+++ main.py
@@ -11,3 +11,22 @@ import aiohttp
 import asyncio
 from selectolax.parser import HTMLParser
 import os
+load_dotenv()  # .env 파일에서 TOKEN 등 불러오기
+run_forever()   # 플라스크 서버 실행 (Render 유지용)
+
+
+
+
+
+color = discord.Color.blue()
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+
+    print(f'봇 로그인됨: {bot.user}')
+    await bot.change_presence(status=discord.Status.idle)
