518abc9b4a89fc418fe3d93d0849572335922c53
topquark2025 <creatortopquark@gmail.com>
2025-05-26 15:56:03 +0900

    utils.py 업데이트

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,8 @@
+import requests
 
+def send_location_to_server(lat, lon):
+    try:
+        response = requests.post("http://YOUR_FLASK_SERVER/loc", json={"lat": lat, "lon": lon})
+        print("전송 완료:", response.status_code)
+    except Exception as e:
+        print("전송 실패:", e)
